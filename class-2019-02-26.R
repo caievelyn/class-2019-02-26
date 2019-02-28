38f909fd11c363b484494ec4f6daa5ad78cd34a4
caievelyn <evelyncai@college.harvard.edu>
2019-02-28 13:42:38 -0500

    automatically download updated version of enrollment registrations

---
+++ class-2019-02-26.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(dplyr)
+library(readxl)
+library(janitor)
+library(devtools)
+library(gt)
+
+download.file("https://registrar.fas.harvard.edu/files/fas-registrar/files/class_enrollment_summary_by_term_2.28.19.xlsx",
+              destfile = "reg_2019.xlsx",
+              mode = "wb")
+
+x_2019 <- read_xlsx("class_enrollment_summary_by_term_2.26.19.xlsx", skip = 3) %>%
+  clean_names() %>%
+  filter(!is.na(course_name)) %>%
+  select(course_title, course_name, u_grad, course_id)
+  
+x_2018 <- read_xlsx("class_enrollment_summary_by_term_03.06.18.xlsx", skip = 2) %>%
+  clean_names() %>%
+  filter(!is.na(course_name)) %>%
+  select(course_title, course_name, u_grad, course_id)
+
+all <- inner_join(x_2019, x_2018, by = "course_id", suffix = c(".2019", ".2018"))
+
+all %>%
+  select(course_title.2019, course_name.2019, u_grad.2018, u_grad.2019) %>%
+  mutate(Change = -(u_grad.2019 - u_grad.2018)) %>%
+  arrange(desc(Change)) %>%
+  slice(1:10) %>%
+  mutate(Change = -Change) %>%
+  gt() %>%
+  tab_header(title = "Biggest Enrollment Decreases in Spring 2019") %>%
+  tab_source_note(
+    source_note = "Data from the Harvard Registrar") %>%
+  cols_label("course_title.2019" = "Number",
+             "course_name.2019" = "Name",
+             "u_grad.2019" = "2019",
+             "u_grad.2018" = "2018")
